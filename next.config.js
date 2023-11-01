2b9abef438999fe37b2345cf00feca65225beff9
ritughiya <ritughiya@gmail.com>
2023-11-01 16:04:25 -0400

    cors

--- next.config.js
+++ next.config.js
@@ -1,10 +1,35 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
+
 const withImages = require("next-images");
 module.exports = withImages();
 
-module.exports = {
+// module.exports = {
+//   reactStrictMode: false,
+//   images: {
+//     domains: ["cdn.sanity.io"],
+//   },
+// };
+
+const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["cdn.sanity.io"],
   },
-};
+  async headers() {
+      return [
+          {
+              // matching all API routes
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+              ]
+          }
+      ]
+  }
+}
+
+module.exports = nextConfig
