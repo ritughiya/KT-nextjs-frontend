a1a39d1adab7eb1910eac26e5f389207d9a5d5be
ritughiya <ritughiya@gmail.com>
2025-10-28 11:11:04 -0400

    g tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,51 @@
 import '../styles/globals.css'
 import Script from 'next/script'
 import { sanityClient, urlFor } from '../sanity'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 
-function MyApp({ Component, pageProps }) {  return (
-(  <>
-          {/* <Script id="analytics1"
-         async type="text/javascript" 
-        src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=SSrDta`}
-      /> */}
+
+function MyApp({ Component, pageProps })  {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Load GA script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+
+      {/* Initialize GA */}
+      <Script
+        id="ga-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
 <Component {...pageProps} />
 
@@ -16,8 +53,7 @@ function MyApp({ Component, pageProps }) {  return (
 
 
 </>
-)  
-  )
+  );
 }
 
 
